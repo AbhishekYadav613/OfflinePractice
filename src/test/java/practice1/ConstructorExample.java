fda9c39be09a045433021f9f09bb05eb410427d8
AbhishekYadav613 <abhishekyadav613@gmail.com>
2024-03-28 20:34:41 +0530

    Edit Constructor

--- src/test/java/practice1/ConstructorExample.java
+++ src/test/java/practice1/ConstructorExample.java
@@ -3,13 +3,16 @@ package practice1;
 public class ConstructorExample {
 
     public static void main(String[] args) {
-        ConstructorExample ex = new ConstructorExample("Ruskin Bond", 55);
+
+        ConstructorExample ex = new ConstructorExample();
     }
 
+    // If parameters are not passed, then this constructor will be executed.
     public ConstructorExample() {
+
         System.out.println("This is a Constructor.");
     }
-
+// If parameters are passed, then this constructor will be executed.
     public ConstructorExample(String author, int age) {
         System.out.println("This is Parameterized Constructor with: " + author + age);
     }
